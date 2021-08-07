8a1c568c63f54274669c3e1cbbdf509126c75b0c
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2021-08-07 14:18:59 +0200

    initial UI and notes API call

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
+  const [notesList, setNotesList] = useState<Array<any>>([]);
+
+  // get notes method
+  async function getNotes() {
+    try {
+      const response = await axios.get("http://localhost:5000/notes");
+      setNotesList(response.data.notes);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  console.log(notesList);
+
   return (
+    //
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Notes Application</div>
+      <div>
+        <button onClick={getNotes}>Click Me!</button>
+      </div>
+      <div>
+        {/* SHOW DETAILS of the first element */}
+        <h4>{notesList[0]?.text}</h4>
+        <h5>{notesList[0]?.link}</h5>
+      </div>
     </div>
   );
 }
